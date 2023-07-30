94f924a40028c952d061cc6095026bbd14b00439
Sulabh Shrestha <Shresthasulabh777@gmail.com>
2023-07-30 12:25:48 +0545

    Populated AboutPage

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -1,5 +1,17 @@
 function AboutPage() {
-    return ( <h1>About Page</h1> );
+    return (
+      <div className="relative">
+        <main>
+          <p>A beautiful website that helps to keep your focus maintained.</p>
+        </main>
+
+        <footer className="fixed bottom-0 bg-yellow-50 w-full py-3">
+          <p>
+            Made with 💜 by Sulabh Shrestha. &copy; All rights reserved 2023.
+          </p>
+        </footer>
+      </div>
+    );
 }
 
 export default AboutPage;
